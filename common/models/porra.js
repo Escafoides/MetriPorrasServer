31b5612659d90aaf6074bdd6ebe40c722850d14e
Pablo Sabater Canseco <pablo.sabater@metrica.es>
2018-02-13 18:42:54 +0100

    Añadidas las apuestas en la porra y empezado el borrado en cascada.

--- common/models/porra.js
+++ common/models/porra.js
@@ -2,4 +2,17 @@
 
 module.exports = function(Porra) {
 
+
+    Porra.deletePorra = function(id, cb) {
+      Porra.findOne({where:{id:id}},function(err,porra){
+            console.log(porra.apuestas);
+            cb(null, porra);
+      });  
+      }
+  
+      Porra.remoteMethod('deletePorra', {
+            accepts: {arg: 'id', type: 'string', required: true},
+            returns: {arg: 'response', type: 'Porra', "root": true},
+            http: {path: '/deletePorra', verb: 'get'}
+      });
 };
